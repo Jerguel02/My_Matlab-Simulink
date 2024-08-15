15ed4cadf4966109821a97325b411e7145ec3b8b
PhamNghia <huunghia25012002@gmail.com>
2024-08-16 01:06:03 +0700

    push code

--- Matlab/Complex_num/EulerSeries_Implement.m
+++ Matlab/Complex_num/EulerSeries_Implement.m
@@ -1,13 +1,14 @@
 clc;
 clear all;
 %Generate a function for the differential equation
-%Differential equation: dy/dx=y'=2x^2+y-2
 %Condition:=<=x<=5; y(0) = 0.1
 %N.B Matlab iterates from 1, not from 0
 %Hence, for the initial value y(0), we will consider it y(1)
 %Number of step: 18
 
-f = @(x,y) 2*x^2+y-2;
+%f = @(x,y) 2*x^2+y-2;
+f_str = input('Enter a fomular: ','s');
+f = @(x,y) subs(str2sym(f_str), {'x', 'y'}, {x, y});
 L = input('Enter lower limit of x: ');
 U = input('Enter upper limit of x: ');
 N = input('Enter the number of step: ');
